64a27d0fcdda24570b0303b329bdbfc64a0eac7d
nicolepap <149177322+nicolepap@users.noreply.github.com>
2023-11-17 12:48:56 -0700

    Create bottomnav.jsx

---
+++ src/components/button/bottomnav.jsx
@@ -0,0 +1,33 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import BottomNavigation from '@mui/material/BottomNavigation';
+import BottomNavigationAction from '@mui/material/BottomNavigationAction';
+
+import FitnessCenterIcon from '@mui/icons-material/FitnessCenter';
+import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone';
+import PersonIcon from '@mui/icons-material/Person';
+
+function SimpleBottomNavigation() {
+  const [value, setValue] = React.useState(0);
+
+  return (
+    <Box sx={{ width: 500 }}>
+      <BottomNavigation
+        showLabels
+        value={value}
+        onChange={(event, newValue) => {
+          setValue(newValue);
+        }}
+      >
+        <BottomNavigationAction label="LogBook" icon={<FitnessCenterIcon />} />
+        <BottomNavigationAction label="Alerts" icon={<NotificationsNoneIcon />} />
+        <BottomNavigationAction label="User" icon={<PersonIcon />} />
+      </BottomNavigation>
+    </Box>
+  );
+}
+
+export default SimpleBottomNavigation;
+
+{/*npm install @mui/icons-material @mui/material @emotion/styled @emotion/react --force*/}
+
