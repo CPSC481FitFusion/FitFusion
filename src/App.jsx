d6fe162ab8d64ed644c3b380debc30e64b78f1b1
racherene <rachelveronikarenegado@gmail.com>
2023-11-20 16:28:12 -0700

    feat: Added auth pages to routing + created files

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,33 @@
-import 'bootstrap/dist/css/bootstrap.css';
-import { StyledEngineProvider} from '@mui/material/styles';
 import './scss/styles.scss';
-
-import { WorkoutPage } from "./pages/LogbookWorkoutPage"
-import { GoalsPage } from "./pages/LogbookGoalsPage"
-import { BodyPage } from "./pages/LogbookBodyPage"
-import { NotFoundPage } from "./pages/NotFoundPage"
-import { SettingsPage } from "./pages/SettingsPage"
+import 'bootstrap/dist/css/bootstrap.css';
 import { AlertsPage } from './pages/AlertsPage';
+import { BodyPage } from "./pages/LogbookBodyPage"
+import { GoalsPage } from "./pages/LogbookGoalsPage"
+import { LoginPage } from "./pages/LoginPage"
+import { NotFoundPage } from "./pages/NotFoundPage"
 import { Route, Routes } from 'react-router-dom';
+import { RegisterPage } from "./pages/RegisterPage"
+import { SettingsPage } from "./pages/SettingsPage"
+import { StyledEngineProvider } from '@mui/material/styles';
+import { WelcomePage } from './pages/WelcomePage';
+import { WorkoutPage } from "./pages/LogbookWorkoutPage"
 
 function App() {
   return (
     <>
-    <StyledEngineProvider injectFirst>
+      <StyledEngineProvider injectFirst>
         <Routes>
-          <Route path="/" element={<WorkoutPage />} />
+          <Route path="/" element={<WelcomePage />} />
+          <Route path="login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/workout" element={<WorkoutPage />} />
           <Route path="/goals" element={<GoalsPage />} />
           <Route path="/body" element={<BodyPage />} />
           <Route path="/alerts" element={<AlertsPage />} />
           <Route path="/settings" element={<SettingsPage />} />
           <Route path="*" element={<NotFoundPage />} />
         </Routes>
-    </StyledEngineProvider>
+      </StyledEngineProvider>
     </>
   )
 }
