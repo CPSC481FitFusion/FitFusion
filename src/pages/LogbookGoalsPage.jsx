502a2a875da010fddfb9bdbd7ffb4aff0f9623ed
nicolepap <149177322+nicolepap@users.noreply.github.com>
2023-11-22 23:11:33 -0700

    Update LogbookGoalsPage.jsx

--- src/pages/LogbookGoalsPage.jsx
+++ src/pages/LogbookGoalsPage.jsx
@@ -1,7 +1,49 @@
+import React, { useState } from 'react';
+import ButtonFilled from "../components/ButtonFilled";
+import EditModal from "../components/Modals/EditModal"; // Import your existing EditModal component
+
 export const GoalsPage = () => {
+    const [isEditModalOpen, setEditModalOpen] = useState(false);
+  
+    const handleOpenEditModal = () => {
+      setEditModalOpen(true);
+    };
+  
+    const handleCloseEditModal = () => {
+      setEditModalOpen(false);
+    };
+  
     return (
-        <>
-        <h1> Goals Page</h1>
-        </>
+      <>
+        <h1 className="header-35">Goals Page</h1>
+        <h2 className="header-25">Add a new goal</h2>
+  
+        {/* Start button */}
+        <ButtonFilled
+          style="background-green"
+          text="Start"
+        />
+  
+        {/* Goal History*/}
+        <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', marginTop: '20px' }}>
+          <p className="general-label">Goal History</p>
+  
+          {/* Right side: Square Edit Details button */}
+          <div className="see-all">
+            <div className="overlap-5">
+              {/* Pass relevant props to the EditModal */}
+              <EditModal
+                isIcon={true} // You can adjust this based on your design
+                editButtonLabel="Edit Details"
+                modalHeader="Edit Goal Details"
+                modalBody="Edit your goal details..."
+                onClickRemove={() => {/* Implement your remove logic */}}
+                onClickSave={() => {/* Implement your save logic */}}
+              />
+            </div>
+          </div>
+        </div>
+      </>
     );
-}
+  };
+
