119fa44deff37d4e06c1b93dd1efcf7fa35c9e28
racherene <rachelveronikarenegado@gmail.com>
2023-11-23 00:15:55 -0700

    feat: Added styling to logbook nav + bottom nav

--- src/components/AppBottomNavigation.jsx
+++ src/components/AppBottomNavigation.jsx
@@ -11,33 +11,36 @@ export const AppBottomNavigation = ({ state }) => {
   const [value, setValue] = React.useState(state);
 
   return (
-    <Box sx={{ position: 'fixed', bottom: 0, left: 0, right: 0, width: '100%', zIndex: 1000 }}>
-      <BottomNavigation
-        showLabels
-        value={value}
-        onChange={(event, newValue) => {
-          setValue(newValue);
-        }}
-      >
-        <BottomNavigationAction
-          component={Link}
-          to="/logbook"
-          value="logbook"
-          label="Logbook"
-          icon={<FitnessCenterIcon />} />
-        <BottomNavigationAction
-          component={Link}
-          to="/alerts"
-          value="alerts"
-          label="Alerts"
-          icon={<NotificationsNoneIcon />} />
-        <BottomNavigationAction
-          component={Link}
-          to="/settings"
-          value="settings"
-          label="Settings"
-          icon={<SettingsIcon />} />
-      </BottomNavigation>
+    <Box className="bottom-nav-container" >
+      <Box sx={{ paddingTop:'10px', borderTop: 1, borderColor: 'divider' }}>
+
+        <BottomNavigation
+          showLabels
+          value={value}
+          onChange={(event, newValue) => {
+            setValue(newValue);
+          }}
+        >
+          <BottomNavigationAction
+            component={Link}
+            to="/logbook"
+            value="logbook"
+            label="Logbook"
+            icon={<FitnessCenterIcon />} />
+          <BottomNavigationAction
+            component={Link}
+            to="/alerts"
+            value="alerts"
+            label="Alerts"
+            icon={<NotificationsNoneIcon />} />
+          <BottomNavigationAction
+            component={Link}
+            to="/settings"
+            value="settings"
+            label="Settings"
+            icon={<SettingsIcon />} />
+        </BottomNavigation>
+      </Box>
     </Box>
   );
 }
--- src/pages/Logbook/LogbookPage.jsx
+++ src/pages/Logbook/LogbookPage.jsx
@@ -1,4 +1,4 @@
-import { Box, Stack, Tab } from "@mui/material";
+import { Box, Stack, Tab, Typography } from "@mui/material";
 import AppBottomNavigation from "../../components/AppBottomNavigation";
 import BasicConfirmationModal from "../../components/Modals/BasicConfirmationModal";
 import ButtonFilled from "../../components/ButtonFilled";
@@ -8,6 +8,7 @@ import React from "react";
 import TextareaInputWithLabel from "../../components/TextareaInputWithLabel";
 import TextInputWithLabel from "../../components/TextInputWithLabel";
 import { TabContext, TabList, TabPanel } from '@mui/lab';
+import Title from "../../components/title";
 
 export const LogbookPage = () => {
     // Get the user default from local storage, for now set at Workout
@@ -19,14 +20,15 @@ export const LogbookPage = () => {
 
     return (
         <>
-            <h1> Workout Page</h1>
+            <Typography className="header-35">Logbook</Typography>
+            <Typography className="normal-text-14">Click an option below to start tracking!</Typography>
             <Box sx={{ width: '100%' }}>
                 <TabContext value={value}>
                     <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
                         <TabList onChange={handleChange} aria-label="Logbook Tab" variant="fullWidth">
-                            <Tab label="Workout" value="1" />
-                            <Tab label="Body" value="2" />
-                            <Tab label="Goals" value="3" />
+                            <Tab className="tab-option" label="Workout" value="1" />
+                            <Tab className="tab-option" label="Body" value="2" />
+                            <Tab className="tab-option" label="Goals" value="3" />
                         </TabList>
                     </Box>
                     <TabPanel value="1" >Workout</TabPanel>
