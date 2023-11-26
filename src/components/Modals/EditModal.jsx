8334ba2cc350de3492927f713d3b27b2082f6c91
Ali Siddiqi <60081630+alisiddiqi@users.noreply.github.com>
2023-11-26 15:30:28 -0700

    added workout cards

--- src/components/Modals/EditModal.jsx
+++ src/components/Modals/EditModal.jsx
@@ -1,93 +1,135 @@
+import React, { useState, useEffect } from 'react';
 import { Modal, ModalClose, Typography, Sheet } from '@mui/joy';
-import React from 'react';
 import ButtonFilled from '../ButtonFilled';
 import { Button, IconButton, Stack } from '@mui/material';
 import EditIcon from '@mui/icons-material/Edit';
 
-const EditModal = ({ 
-    isIcon, editButtonLabel, modalHeader, modalBody, onClickRemove, onClickSave }) => {
-    const [open, setOpen] = React.useState(false);
+const EditModal = ({ isIcon, editButtonLabel, modalHeader, modalBody, onClickRemove, onClickSave }) => {
+  const [open, setOpen] = React.useState(false);
+  const [editedDetails, setEditedDetails] = useState({
+    workoutName: '',
+    date: '',
+    startTime: '',
+    notes: '',
+  });
 
-    let button = (<></>);
-    if (isIcon) {
-        button = (
-            <IconButton onClick={() => setOpen(true)}>
-                <EditIcon />
-            </IconButton>
-        );
+  useEffect(() => {
+    // Update the editedDetails state when the modal is opened
+    if (open) {
+      setEditedDetails({
+        workoutName: modalBody.props.children[0].props.value || '',
+        date: modalBody.props.children[1].props.value || '',
+        startTime: modalBody.props.children[2].props.value || '',
+        notes: modalBody.props.children[3].props.value || '',
+      });
     }
-    else {
-        button = (
-            <Button
-                variant='outlined'
-                className='purple-border-button'
-                onClick={() => setOpen(true)}
-            >
-                {editButtonLabel}
-            </Button>
-        );
-    }
+  }, [open, modalBody]);
 
-    const handleClose = (event, reason) => {
-        if (reason !== 'backdropClick') {
-            setOpen(false)
-        }
-    }
-    return (
-        <>
-            {button}
-            <Modal
-                aria-labelledby="modal-title"
-                aria-describedby="modal-desc"
-                open={open}
-                onClose={handleClose}
-                sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}
-            >
-                <Sheet
-                    variant="outlined"
-                    sx={{
-                        width: '90%',
-                        borderRadius: 'md',
-                        p: 3,
-                        boxShadow: 'lg',
-                    }}
-                >
-                    <ModalClose variant="outlined" sx={{ m: 1 }} />
-                    <Stack direction="column" alignItems="center">
-                        <Typography
-                            component="h2"
-                            id="modal-title"
-                            level="h4"
-                            className='header-25'
-                            mb={1}
-                        >
-                            {modalHeader}
-                        </Typography>
-                        <Typography
-                            id="modal-desc"
-                            textColor="text.tertiary"
-                            className="text-center"
-                        >
-                            {modalBody}
-                        </Typography>
-                        <Stack direction="row" spacing={1} className='w-100 mt-3'>
-                            <Button
-                                variant='outlined'
-                                className='red-border-button'
-                                onClick={() => setOpen(true)}
-                            >
-                                Remove
-                            </Button>
-                            <ButtonFilled
-                                text="Save"
-                                style="background-green"
-                                onClick={() => 
-                                setOpen(false)} />
-                        </Stack>
-                    </Stack>
-                </Sheet>
-            </Modal>
-        </>
+  let button = (<></>);
+  if (isIcon) {
+    button = (
+      <IconButton onClick={() => setOpen(true)}>
+        <EditIcon />
+      </IconButton>
     );
-}
+  } else {
+    button = (
+      <Button
+        variant='outlined'
+        className='purple-border-button'
+        onClick={() => setOpen(true)}
+      >
+        {editButtonLabel}
+      </Button>
+    );
+  }
+
+  const handleClose = (event, reason) => {
+    if (reason !== 'backdropClick') {
+      setOpen(false);
+    }
+  };
+
+  const handleInputChange = (label, value) => {
+    setEditedDetails((prevDetails) => ({ ...prevDetails, [label]: value }));
+  };
+
+  const handleRemove = () => {
+    if (onClickRemove) {
+      onClickRemove();
+    }
+    // Additional logic for removing item if needed
+    setOpen(false);
+  };
+
+  const handleSave = () => {
+    // Call the onClickSave function passed as a prop and pass the edited details
+    if (onClickSave) {
+      onClickSave(editedDetails);
+    }
+    // Additional logic for saving changes if needed
+    setOpen(false);
+  };
+
+  return (
+    <>
+      {button}
+      <Modal
+        aria-labelledby="modal-title"
+        aria-describedby="modal-desc"
+        open={open}
+        onClose={handleClose}
+        sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}
+      >
+        <Sheet
+          variant="outlined"
+          sx={{
+            width: '90%',
+            borderRadius: 'md',
+            p: 3,
+            boxShadow: 'lg',
+          }}
+        >
+          <ModalClose variant="outlined" sx={{ m: 1 }} />
+          <Stack direction="column" alignItems="center">
+            <Typography
+              component="h2"
+              id="modal-title"
+              level="h4"
+              className='header-25'
+              mb={1}
+            >
+              {modalHeader}
+            </Typography>
+            <div>
+              {/* Render the modalBody JSX passed as a prop */}
+              {React.Children.map(modalBody.props.children, (child, index) => {
+                // Clone each child element with added onChange prop
+                return React.cloneElement(child, {
+                  onChange: (value) => handleInputChange(child.props.label, value),
+                  value: editedDetails[child.props.label] || '',
+                });
+              })}
+            </div>
+            <Stack direction="row" spacing={1} className='w-100 mt-3'>
+              <Button
+                variant='outlined'
+                className='red-border-button'
+                onClick={handleRemove}
+              >
+                Remove
+              </Button>
+              <ButtonFilled
+                text="Save"
+                style="background-green"
+                onClick={handleSave}
+              />
+            </Stack>
+          </Stack>
+        </Sheet>
+      </Modal>
+    </>
+  );
+};
+
 export default EditModal;
--- src/pages/Logbook/WorkoutTab/WorkoutTab.jsx
+++ src/pages/Logbook/WorkoutTab/WorkoutTab.jsx
@@ -6,6 +6,11 @@ import BasicConfirmationModal from "../../../components/Modals/BasicConfirmation
 import WorkoutDetails from "./WorkoutDetails";
 import WorkoutExerciseCard from "./WorkoutExerciseCard";
 import Container from "../../../components/Container";
+import { Card } from '@mui/material';
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
+import CloseIcon from '@mui/icons-material/Close';
+import IconButton from '@mui/material/IconButton';
 
 const WorkoutTab = () => {
     const [open, setOpen] = React.useState(false);
@@ -63,6 +68,69 @@ const WorkoutTab = () => {
                     </Stack>
                 </Sheet>
             </Modal>
+            <div
+               style={{margin: '5%'}}>
+
+        <Card sx={{ maxWidth: 600 }}>
+            <CardContent>
+                <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+                <Typography variant='h3' gutterBottom>
+                   Arm Day
+                </Typography>
+                <IconButton > <CloseIcon /> </IconButton>
+                </div>
+                <Typography variant="h4" component="div">
+                Oct 24, 2023
+                </Typography>
+                <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                Hammer Curls (Dumbbell)
+                </Typography>
+                <Typography variant="body1">
+                3 Sets  5 reps 10lbs 
+                <br />
+                </Typography>
+                <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                Lateral Pulldown Cable
+                </Typography>
+                <Typography variant="body1">
+                2 Sets  8 reps 55-70lbs 
+                <br />
+                </Typography>
+            </CardContent>
+        </Card>
+
+        <p></p>
+        <Card sx={{ maxWidth: 600 }}>
+            <CardContent>
+                <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+                <Typography variant='h3' gutterBottom>
+                   Leg Day
+                </Typography>
+                <IconButton > <CloseIcon /> </IconButton>
+                </div>
+                <Typography variant="h4" component="div">
+                Oct 23, 2023
+                </Typography>
+                <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                Leg Curls
+                </Typography>
+                <Typography variant="body1">
+                3 Sets  8-10 reps 100lbs 
+                <br />
+                </Typography>
+                <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                Leg Press 
+                </Typography>
+                <Typography variant="body1">
+                2 Sets  8 reps 200lbs 
+                <br />
+                </Typography>
+            </CardContent>
+        </Card>
+
+
+            </div>
+
         </>
     );
 };
