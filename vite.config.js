eb8d3dae6ea7d8777db3094fa7b48b3fd4bd90d2
nicolepap <papucnic@gmail.com>
2023-12-02 13:57:29 -0700

    edits

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,8 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/CPSC481FitFusion/FitFusion/",
+  base: "/FitFusion/",
   plugins: [react()],
-  build: {
-    outDir: 'dist',
-    base: "/CPSC481FitFusion/FitFusion/"
-  },
   server: {
     port: 3000,
   },
