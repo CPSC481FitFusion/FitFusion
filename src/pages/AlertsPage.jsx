43e914392f127347790d779ce1bb0e3130514426
Jitaksha <65079053+jitaksha2001@users.noreply.github.com>
2023-12-04 12:03:29 -0700

    Updated the UI

--- src/pages/AlertsPage.jsx
+++ src/pages/AlertsPage.jsx
@@ -1,4 +1,3 @@
-import AppBottomNavigation from "../components/AppBottomNavigation";
 import React, { useState } from 'react';
 import ButtonFilled from '../components/ButtonFilled';
 import EditModal from '../components/Modals/EditModal';
@@ -6,140 +5,141 @@ import { Modal, Sheet, Stack } from '@mui/joy';
 import Container from '../components/Container';
 import AlertDetails from '../Pages/Logbook/AlertTab/Alertdetails';
 import BasicConfirmationModal from "../components/modals/basicConfirmationModal";
+import AppBottomNavigation from "../components/AppBottomNavigation";
 
 export const AlertsPage = () => {
-    const [isEditModalOpen, setEditModalOpen] = useState(false);
-    const [isStartModalOpen, setStartModalOpen] = useState(false);
+  const [isEditModalOpen, setEditModalOpen] = useState(false);
+  const [isStartModalOpen, setStartModalOpen] = useState(false);
 
-    const handleOpenEditModal = () => {
-        setEditModalOpen(true);
-    };
+  const handleOpenEditModal = () => {
+    setEditModalOpen(true);
+  };
 
-    const handleCloseEditModal = () => {
-        setEditModalOpen(false);
-    };
+  const handleCloseEditModal = () => {
+    setEditModalOpen(false);
+  };
 
-    const handleOpenStartModal = () => {
-        setStartModalOpen(true);
-    };
+  const handleOpenStartModal = () => {
+    setStartModalOpen(true);
+  };
 
-    const handleCloseStartModal = () => {
-        setStartModalOpen(false);
-    };
+  const handleCloseStartModal = () => {
+    setStartModalOpen(false);
+  };
 
-    const randomNotifications = [
-        "Don't forget to drink water!",
-        "Time to water your plants!",
-        "Take a break and stretch!",
-        "Reminder: Complete your daily tasks!",
-        // Add more random notifications as needed
-    ];
+  const randomNotifications = [
+    "Don't forget to drink water!",
+    "Time to water your plants!",
+    "Take a break and stretch!",
+    "Reminder: Complete your daily tasks!",
+    // Add more random notifications as needed
+  ];
 
-    // Function to generate a random notification
-    const getRandomNotification = () => {
-        const randomIndex = Math.floor(Math.random() * randomNotifications.length);
-        return randomNotifications[randomIndex];
-    };
+  // Function to generate a random notification
+  const getRandomNotification = () => {
+    const randomIndex = Math.floor(Math.random() * randomNotifications.length);
+    return randomNotifications[randomIndex];
+  };
 
-    // Get current date
-    const currentDate = new Date();
-    const formattedDate = currentDate.toDateString();
+  // Get current date
+  const currentDate = new Date();
+  const formattedDate = currentDate.toLocaleDateString('en-US', { weekday: 'long', day: 'numeric', month: 'long', year: 'numeric' });
+  const dayAndDate = currentDate.toLocaleDateString('en-US', { weekday: 'long', day: 'numeric', month: 'long', year: 'numeric' });
 
-    return (
-        <>
-            <h6 className='general-label'> Create a New Reminder</h6>
+  return (
+    <>
+      {/* Heading and Date */}
+      <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+        <div>
+          <h1>Alerts</h1>
+        </div>
+        <p><strong>{formattedDate}</strong></p>
+      </div>
 
-            {/* Display current date */}
-            <p>{formattedDate}</p>
+      {/* Create a New Reminder */}
+      <h6 className='general-label'> <em>Create a New Reminder</em></h6>
 
-            {/* Start button */}
-            <ButtonFilled
-                style="background-green"
-                text="Start"
-                onClick={handleOpenStartModal}
+      {/* Start button */}
+      <ButtonFilled
+        style="background-green"
+        text="Start"
+        onClick={handleOpenStartModal}
+      />
+
+      {/* Start Modal */}
+      <Modal
+        aria-labelledby="modal-title"
+        aria-describedby="modal-desc"
+        open={isStartModalOpen}
+        onClose={handleCloseStartModal}
+        sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}
+      >
+        <Sheet
+          variant="outlined"
+          className="full-page-modal-container"
+        >
+          {/* Additional content can be added here */}
+          <Container
+            style={"background-blue-light m-0"}
+            children={
+              <>
+                <AlertDetails />
+              </>
+            }
+          />
+          <Stack spacing={20} direction="row" className="horizontal-stack">
+            <BasicConfirmationModal
+              buttonStyle={"background-orange"}
+              openModalButtonLabel={"Remove "}
+              modalHeader={" Go back"}
+              modalBody={"Are you sure you want to not proceed with setting up a new reminder?"}
+              modalConfirmationButtonLabel={" Remove"}
+              actionOnClick={() => handleCloseStartModal()}
             />
-            {/* Start Modal */}
-            <Modal
-                aria-labelledby="modal-title"
-                aria-describedby="modal-desc"
-                open={isStartModalOpen}
-                onClose={handleCloseStartModal}
-                sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}
-            >
-                <Sheet
-                    variant="outlined"
-                    className="full-page-modal-container"
-                >
-                    {/* Additional content can be added here */}
-                    <Container
-                        style={"background-blue-light m-0"}
-                        children={
-                            <>
-                                <AlertDetails />
-                            </>
-                        }
-                    />
-                    <Stack spacing={20} direction="row" className="horizontal-stack">
-                        <BasicConfirmationModal
-                            buttonStyle={"background-orange"}
-                            openModalButtonLabel={"Remove "}
-                            modalHeader={" Go back"}
-                            modalBody={"Are you sure you want to not proceed with setting up a new reminder?"}
-                            modalConfirmationButtonLabel={" Remove"}
-                            actionOnClick={() => handleCloseStartModal()}
-                        />
-                        <BasicConfirmationModal
-                            buttonStyle={"background-green"}
-                            openModalButtonLabel={"Save"}
-                            modalHeader={" Setting up"}
-                            modalBody={"Are you sure you want to set up a new reminder?"}
-                            modalConfirmationButtonLabel={" Save"}
-                            actionOnClick={() => handleCloseStartModal()}
-                        />
-                    </Stack>
-                </Sheet>
-            </Modal>
+            <BasicConfirmationModal
+              buttonStyle={"background-green"}
+              openModalButtonLabel={"Save"}
+              modalHeader={" Setting up"}
+              modalBody={"Are you sure you want to set up a new reminder?"}
+              modalConfirmationButtonLabel={" Save"}
+              actionOnClick={() => handleCloseStartModal()}
+            />
+          </Stack>
+        </Sheet>
+      </Modal>
 
-            {/* Reminder History */}
-            <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', marginTop: '20px' }}>
-                <p className="general-label"> Reminders </p>
-                <p className="general-label"> Check off the reminder to mark of as complete! </p>
+      {/* Reminder History */}
+      <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', marginTop: '20px' }}>
+        <p className="general-label"> Reminders </p>
+        {/* Check off the reminder to mark it as complete! */}
+        <p className="general-label" style={{ marginTop: '10px' }}> Check off the reminder to mark it as complete! </p>
+        {/* Right side: Square Edit Details button */}
+        <div className="see-all">
+          <div className="overlap-5">
+            {/* Pass relevant props to the EditModal */}
+            <EditModal
+              isIcon={true}
+              isOpen={false}
+              editButtonLabel="Edit Reminders"
+              modalHeader="Edit Reminder Details"
+              // modalBody="Attending Zumba Class"
+              onClickRemove={() => {/* Implement your remove logic */ }}
+              onClickSave={() => {/* Implement your save logic */ }}
+            />
+          </div>
+        </div>
+      </div>
 
-                
+      {/* Display random notifications as toast notifications */}
+      <div style={{ marginTop: '20px' }}>
+        {randomNotifications.map((notification, index) => (
+          <div key={index} className="toast-notification">
+            {notification}
+          </div>
+        ))}
+      </div>
 
-                {/* Right side: Square Edit Details button */}
-                <div className="see-all">
-                    <div className="overlap-5">
-                        {/* Pass relevant props to the EditModal */}
-                        <EditModal
-                            isIcon={true}
-                            isOpen={false}
-                            editButtonLabel="Edit Remiders"
-                            modalHeader="Edit Reminder Details"
-                            // modalBody="Attending Zumba Class"
-                            onClickRemove={() => {/* Implement your remove logic */ }}
-                            onClickSave={() => {/* Implement your save logic */ }}
-                        />
-                    </div>
-                </div>
-
-
-                 {/* Display random notifications as checkboxes */}
-            
-            </div>
-
-            <div style={{ marginTop: '20px' }}>
-                <p className="general-label">Random Notifications</p>
-                {randomNotifications.map((notification, index) => (
-                    <div key={index} style={{ display: 'flex', alignItems: 'center', marginBottom: '10px' }}>
-                        <input type="checkbox" id={`checkbox-${index}`} />
-                        <label htmlFor={`checkbox-${index}`} style={{ marginLeft: '10px' }}>
-                            {notification}
-                        </label>
-                    </div>
-                ))}
-            </div>
-            <AppBottomNavigation state={"alerts"} />
-        </>
-    );
+      <AppBottomNavigation state={"alerts"} />
+    </>
+  );
 };
