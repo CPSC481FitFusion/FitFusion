06edd000ae1efab66a932a6355bd8c72520c2ca7
racherene <rachelveronikarenegado@gmail.com>
2023-12-05 03:10:27 -0700

    Added body composition validation

--- src/pages/Logbook/BodyTab/BodyDetails.jsx
+++ src/pages/Logbook/BodyTab/BodyDetails.jsx
@@ -9,49 +9,65 @@ import ButtonFilled from "../../../components/ButtonFilled";
 import Container from "../../../components/Container";
 import { RequiredInputLabel } from "../../../components/RequiredInputLabel";
 import TextInputWithLabel from "../../../components/TextInputWithLabel";
+import ErrorSnackbar from "../../../components/ErrorSnackbar";
 
 const BodyDetails = ({ body, onUpdate, onClose, isOpen }) => {
-    const [modalOpen, setModalOpen] = useState(isOpen); // State for modal
-    const [tempDetails, setTempDetails] = useState({
-      // Temporary state for body composition details, used while editing
-      date: body?.date || new Date(),
-      weight: body?.weight || "",
-      waistCircumference: body?.waistCircumference || "",
-      hipCircumference: body?.hipCircumference || "",
-      armCircumference: body?.armCircumference || "",
-      thighCircumference: body?.thighCircumference || "",
-    });
+  const [modalOpen, setModalOpen] = useState(isOpen); // State for modal
+  const [tempDetails, setTempDetails] = useState({
+    // Temporary state for body composition details, used while editing
+    date: body?.date || new Date(),
+    weight: body?.weight || "",
+    waistCircumference: body?.waistCircumference || "",
+    hipCircumference: body?.hipCircumference || "",
+    armCircumference: body?.armCircumference || "",
+    thighCircumference: body?.thighCircumference || "",
+  });
+  const [snackbarOpen, setSnackbarOpen] = useState(false); // State for showing the invalid login info snackbar
+  const [snackbarMessage, setSnackbarMessage] = useState(false);
 
-    useEffect(() => {
-      // Use effect for handing modal state. Allows for modal to open when starting new body composition.
-      setModalOpen(isOpen);
-    }, [isOpen]);
+  const handleSnackbarClose = () => {
+    // Handle Close for inalid login error snackbar
+    setSnackbarOpen(false);
+  };
 
-     // Handler for closing modal
-    const handleModalClose = (event, reason) => {
-      if (reason && reason == "backdropClick") return;
-      onClose();
-      setModalOpen(false);
-    };
+  useEffect(() => {
+    // Use effect for handing modal state. Allows for modal to open when starting new body composition.
+    setModalOpen(isOpen);
+  }, [isOpen]);
 
-    const handleModalSave = () => {
-        onUpdate({ ...body, ...tempDetails });
-        setModalOpen(false);
-      };
+  // Handler for closing modal
+  const handleModalClose = (event, reason) => {
+    if (reason && reason == "backdropClick") return;
+    onClose();
+    setModalOpen(false);
+  };
 
-    return (
-      <>
-        <Stack direction="row" className="horizontal-stack pb-1">
-          <Typography className="header-20 align-bottom">
-                    Body Composition
-          </Typography>
-          <Button
-            variant="outlined"
-            className="purple-border-button"
-            onClick={() => setModalOpen(true)}
-          >
-            Edit
-          </Button>
+  const handleModalSave = () => {
+    // Validation: Ensure date and at least one metric is provided
+    const hasAtLeastOneMetric = tempDetails.weight || tempDetails.waistCircumference || tempDetails.hipCircumference || tempDetails.armCircumference || tempDetails.thighCircumference;
+    if (!hasAtLeastOneMetric) {
+      // Handle error: No metric provided
+      setSnackbarOpen(true);
+      setSnackbarMessage("At least 1 body composition metric must be inputted to save.")
+      return;
+    }
+    onUpdate({ ...body, ...tempDetails });
+    setModalOpen(false);
+  };
+
+  return (
+    <>
+      <Stack direction="row" className="horizontal-stack pb-1">
+        <Typography className="header-20 align-bottom">
+          Body Composition
+        </Typography>
+        <Button
+          variant="outlined"
+          className="purple-border-button"
+          onClick={() => setModalOpen(true)}
+        >
+          Edit
+        </Button>
         {/* Modal for editing body composition details */}
         <Modal
           open={modalOpen}
@@ -67,10 +83,11 @@ const BodyDetails = ({ body, onUpdate, onClose, isOpen }) => {
             variant="outlined"
             sx={{ width: "90%", borderRadius: "md", p: 3, boxShadow: "lg" }}
           >
-            <ModalClose
-              onClick={handleModalClose}
-              variant="outlined"
-              sx={{ m: 1 }}
+            {/* On invalid empty attempt */}
+            <ErrorSnackbar
+              isOpen={snackbarOpen}
+              snackbarMessage={snackbarMessage}
+              onClose={handleSnackbarClose}
             />
             <Stack className="w-100" direction="column" alignItems="center">
               <Typography
@@ -87,13 +104,13 @@ const BodyDetails = ({ body, onUpdate, onClose, isOpen }) => {
                   <LocalizationProvider dateAdapter={AdapterDateFns}>
                     <div>
                       <RequiredInputLabel label={"Date"} />
-                        <DatePicker
-                          value={tempDetails.date}
-                          className="time-picker"
-                          onChange={(newDate) =>
-                            setTempDetails({ ...tempDetails, date: newDate })
-                          }
-                        />
+                      <DatePicker
+                        value={tempDetails.date}
+                        className="time-picker"
+                        onChange={(newDate) =>
+                          setTempDetails({ ...tempDetails, date: newDate })
+                        }
+                      />
                     </div>
                   </LocalizationProvider>
                   <TextInputWithLabel
@@ -105,19 +122,19 @@ const BodyDetails = ({ body, onUpdate, onClose, isOpen }) => {
                     }
                   />
                   <TextInputWithLabel
-                  bindValue={tempDetails.waistCircumference}
-                  label={"Waist Circumference (cm)"}
-                  placeholder={"Enter value"}
-                  onInputChange={(e) =>
-                    setTempDetails({ ...tempDetails, waistCircumference: e.target.value })
-                  }
+                    bindValue={tempDetails.waistCircumference}
+                    label={"Waist Circumference (cm)"}
+                    placeholder={"Enter value"}
+                    onInputChange={(e) =>
+                      setTempDetails({ ...tempDetails, waistCircumference: e.target.value })
+                    }
                   />
                   <TextInputWithLabel
                     bindValue={tempDetails.hipCircumference}
                     label={"Hip Circumference (cm)"}
                     placeholder={"Enter value"}
                     onInputChange={(e) =>
-                    setTempDetails({ ...tempDetails, hipCircumference: e.target.value })
+                      setTempDetails({ ...tempDetails, hipCircumference: e.target.value })
                     }
                   />
                   <TextInputWithLabel
@@ -125,7 +142,7 @@ const BodyDetails = ({ body, onUpdate, onClose, isOpen }) => {
                     label={"Arm Circumference (cm)"}
                     placeholder={"Enter value"}
                     onInputChange={(e) =>
-                    setTempDetails({ ...tempDetails, armCircumference: e.target.value })
+                      setTempDetails({ ...tempDetails, armCircumference: e.target.value })
                     }
                   />
                   <TextInputWithLabel
@@ -133,72 +150,72 @@ const BodyDetails = ({ body, onUpdate, onClose, isOpen }) => {
                     label={"Thigh Circumference (cm)"}
                     placeholder={"Enter value"}
                     onInputChange={(e) =>
-                     setTempDetails({ ...tempDetails, thighCircumference: e.target.value })
+                      setTempDetails({ ...tempDetails, thighCircumference: e.target.value })
                     }
                   />
                 </Stack>
               </Grid>
               <Stack className="w-100">
-                  <ButtonFilled
-                    text="Save"
-                    style="background-green"
-                    onClick={handleModalSave}
-                  />
+                <ButtonFilled
+                  text="Save"
+                  style="background-green"
+                  onClick={handleModalSave}
+                />
               </Stack>
             </Stack>
           </Sheet>
         </Modal>
-       </Stack>
-         <Container
-            style={"background-purple-light d-flex align-items-start"}>
-                    <Typography fontWeight={"bold"}>
-                      {tempDetails.date ? format(tempDetails.date, "PP") : ""}
-                    </Typography>
-                    <div style={{marginTop: '5px', marginBottom: '5px'}}>
-                    <Typography fontSize={14}>
-                        Notes
-                    </Typography>
-                    </div>
-            <Stack spacing={2} direction="row" className="pb-2">
-                <Stack direction="column">
-                    <Typography fontStyle="italic">
-                        Body Weight:
-                    </Typography>
-                    <Typography fontStyle="italic">
-                        Waist Circumference:
-                    </Typography>
-                    <Typography fontStyle="italic">
-                        Hip Circumference:
-                    </Typography>
-                    <Typography fontStyle="italic">
-                        Arm Circumference:
-                    </Typography>
-                    <Typography fontStyle="italic">
-                        Thigh Circumference:
-                    </Typography>
-                    
-                </Stack>
-                <Stack direction="column">
-                    <Typography>
-                        {tempDetails?.weight || 0} lbs
-                    </Typography>
-                    <Typography>
-                        {tempDetails?.waistCircumference || 0} cm
-                    </Typography>
-                    <Typography>
-                        {tempDetails?.hipCircumference || 0} cm
-                    </Typography>
-                    <Typography>
-                        {tempDetails?.armCircumferenc || 0} cm
-                    </Typography>
-                    <Typography>
-                        {tempDetails?.thighCircumference || 0} cm
-                    </Typography>
-                </Stack>
-            </Stack>
-            </Container>
-        </>
-    );
+      </Stack>
+      <Container
+        style={"background-purple-light d-flex align-items-start"}>
+        <Typography fontWeight={"bold"}>
+          {tempDetails.date ? format(tempDetails.date, "PP") : ""}
+        </Typography>
+        <div style={{ marginTop: '5px', marginBottom: '5px' }}>
+          <Typography fontSize={14}>
+            Notes
+          </Typography>
+        </div>
+        <Stack spacing={2} direction="row" className="pb-2">
+          <Stack direction="column">
+            <Typography fontStyle="italic">
+              Body Weight:
+            </Typography>
+            <Typography fontStyle="italic">
+              Waist Circumference:
+            </Typography>
+            <Typography fontStyle="italic">
+              Hip Circumference:
+            </Typography>
+            <Typography fontStyle="italic">
+              Arm Circumference:
+            </Typography>
+            <Typography fontStyle="italic">
+              Thigh Circumference:
+            </Typography>
+
+          </Stack>
+          <Stack direction="column">
+            <Typography>
+              {tempDetails?.weight || 0} lbs
+            </Typography>
+            <Typography>
+              {tempDetails?.waistCircumference || 0} cm
+            </Typography>
+            <Typography>
+              {tempDetails?.hipCircumference || 0} cm
+            </Typography>
+            <Typography>
+              {tempDetails?.armCircumferenc || 0} cm
+            </Typography>
+            <Typography>
+              {tempDetails?.thighCircumference || 0} cm
+            </Typography>
+          </Stack>
+        </Stack>
+      </Container>
+    </>
+  );
 };
 
 export default BodyDetails;
