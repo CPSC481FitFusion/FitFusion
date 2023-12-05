2e8d91af148eacdd272a32e69ed55dab6e5c9cff
Rachel Renegado <77893358+racherene@users.noreply.github.com>
2023-12-05 04:34:17 -0700

    Body Composition Fixes (#66)
    * Added different data per user for workout history
    
    * Addressing purple box remaining UI issue
    
    * Finalizing resolution of UI issues I found for History  (#55) (#56)
    
    * Added different data per user for workout history
    
    * Addressing purple box remaining UI issue
    
    * Updated settings page
    
    * Updated mui radio button styling
    
    * Add workout validation when no exercise
    
    * Fixing closing of modal issue
    
    * Updated readme to follow project requirements
    
    * Update for read me and auto closing workout modal ui bug (#58) (#59)
    
    * Added different data per user for workout history
    
    * Addressing purple box remaining UI issue
    
    * Finalizing resolution of UI issues I found for History  (#55) (#56)
    
    * Added different data per user for workout history
    
    * Addressing purple box remaining UI issue
    
    * Updated settings page
    
    * Updated mui radio button styling
    
    * Add workout validation when no exercise
    
    * Fixing closing of modal issue
    
    * Updated readme to follow project requirements
    
    * Implimenting register functionality
    
    * Updated Register to navigate to login screen
    
    * Changes to register (#63) (#64)
    
    * Added different data per user for workout history
    
    * Addressing purple box remaining UI issue
    
    * Finalizing resolution of UI issues I found for History  (#55) (#56)
    
    * Added different data per user for workout history
    
    * Addressing purple box remaining UI issue
    
    * Updated settings page
    
    * Updated mui radio button styling
    
    * Add workout validation when no exercise
    
    * Fixing closing of modal issue
    
    * Updated readme to follow project requirements
    
    * Update for read me and auto closing workout modal ui bug (#58) (#59)
    
    * Added different data per user for workout history
    
    * Addressing purple box remaining UI issue
    
    * Finalizing resolution of UI issues I found for History  (#55) (#56)
    
    * Added different data per user for workout history
    
    * Addressing purple box remaining UI issue
    
    * Updated settings page
    
    * Updated mui radio button styling
    
    * Add workout validation when no exercise
    
    * Fixing closing of modal issue
    
    * Updated readme to follow project requirements
    
    * Implimenting register functionality
    
    * Updated Register to navigate to login screen
    
    * Fixing merge changes
    
    * Added body composition validation
    
    * Changes to Body comp
    
    * Finalizing deletion of body composition
    
    * Fixes from bug report

--- src/pages/Logbook/BodyTab/BodyTab.jsx
+++ src/pages/Logbook/BodyTab/BodyTab.jsx
@@ -1,34 +1,55 @@
 import { Modal, Sheet } from '@mui/joy';
-import { Stack, Typography } from '@mui/material';
-import React, { useState } from 'react';
+import { Grid, Stack, Typography } from '@mui/material';
+import { DatePicker } from "@mui/x-date-pickers";
+import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { useState } from 'react';
 import ButtonFilled from "../../../components/ButtonFilled";
-import BasicConfirmationModal from '../../../components/modals/basicConfirmationModal';
+import Container from "../../../components/Container";
+import ErrorSnackbar from "../../../components/ErrorSnackbar";
+import { RequiredInputLabel } from "../../../components/RequiredInputLabel";
+import TextNumberInputWithLabel from "../../../components/TextNumberInputWithLabel";
 import { getCurrentUsername } from '../../../utils/userUtils';
 import BodyCompositionCard from "./BodyCompositionCard";
-import BodyDetails from "./BodyDetails";
-import Container from "../../../components/Container";
+import BasicConfirmationModal from '../../../components/modals/basicConfirmationModal';
 
 const BodyTab = () => {
-    const [open, setOpen] = useState(false);
-    const [detailsModalOpen, setDetailsModalOpen] = useState(false);
-    const [tempBodyComposition, setTempBodyComposition] = useState(null);
-
-    // Hardcoded body composition data
-    const [hardcodedData, setHardcodedData] = useState({
-        "johndoe": [
-            { id: 1, date: new Date('2023-01-01'), weight: 180, waistCircumference: 32, hipCircumference: 38, armCircumference: 12, thighCircumference: 20 },
-            // ... more data for johndoe
-        ],
-        "bodybuilder": [
-            { id: 2, date: new Date('2023-02-01'), weight: 200, waistCircumference: 34, hipCircumference: 40, armCircumference: 14, thighCircumference: 22 },
-            // ... more data for bodybuilder
-        ],
-        // ... data for other users
+    const [snackbarOpen, setSnackbarOpen] = useState(false);
+    const [snackbarMessage, setSnackbarMessage] = useState('');
+    const [modalOpen, setModalOpen] = useState(false);
+    const [tempDetails, setTempDetails] = useState({
+        date: new Date(),
+        weight: '',
+        waistCircumference: '',
+        hipCircumference: '',
+        armCircumference: '',
+        thighCircumference: '',
     });
 
-    const startNewBodyComposition = () => {
-        setTempBodyComposition({
-            id: Date.now(),
+    // Hardcoded body composition data
+    const hardcodedData = {
+        "johndoe": [
+            { id: 5, date: new Date('2023-12-07'), weight: 180, waistCircumference: 32 },
+            { id: 4, date: new Date('2023-12-03'), armCircumference: 13, thighCircumference: 21 },
+            { id: 3, date: new Date('2023-12-01'), weight: 180, waistCircumference: 32, hipCircumference: 38, armCircumference: 12, thighCircumference: 20 },
+            { id: 2, date: new Date('2023-11-27'), weight: 185, waistCircumference: 33 }, // Partial data
+            { id: 1, date: new Date('2023-11-21'), armCircumference: 13, thighCircumference: 21 }, // Partial data
+        ],
+        "bodybuilder": [
+            { id: 4, date: new Date('2023-12-07'), weight: 200, hipCircumference: 40 },
+            { id: 3, date: new Date('2023-12-05'), weight: 200, waistCircumference: 34, hipCircumference: 40, armCircumference: 14, thighCircumference: 22 },
+            { id: 2, date: new Date('2023-11-30'), weight: 205, hipCircumference: 41, thighCircumference: 23 }, // Partial data
+            { id: 1, date: new Date('2023-11-27'), waistCircumference: 35 }, // Partial data
+        ],
+        // ... data for other users
+    };
+
+    const handleSnackbarClose = () => setSnackbarOpen(false);
+
+    const handleModalClose = () => setModalOpen(false);
+
+    const handleCancel = () => {
+        setTempDetails({
             date: new Date(),
             weight: '',
             waistCircumference: '',
@@ -36,25 +57,21 @@ const BodyTab = () => {
             armCircumference: '',
             thighCircumference: '',
         });
-        setDetailsModalOpen(true);
-        setOpen(true);
+        setModalOpen(false);
     };
 
-    const finishBodyCompositionOnClick = () => {
-        if (tempBodyComposition && tempBodyComposition.weight) {
-            const username = getCurrentUsername();
-            const updatedData = { ...hardcodedData };
-            updatedData[username] = [...(updatedData[username] || []), tempBodyComposition];
-            setHardcodedData(updatedData);
+    const handleModalSave = () => {
+        // Validation
+        if (!tempDetails.weight && !tempDetails.waistCircumference && !tempDetails.hipCircumference && !tempDetails.armCircumference && !tempDetails.thighCircumference) {
+            setSnackbarOpen(true);
+            setSnackbarMessage("At least 1 body composition metric must be inputted to save.");
+            return;
         }
-        setTempBodyComposition(null);
-        setOpen(false);
-        setDetailsModalOpen(false);
-    };
 
-    const handleClose = () => {
-        setOpen(false);
-        setDetailsModalOpen(false);
+        // Save the data
+        // ... Saving logic (e.g., updating the state or local storage)
+
+        setModalOpen(false);
     };
 
     const user = getCurrentUsername();
@@ -65,49 +82,124 @@ const BodyTab = () => {
             <ButtonFilled
                 style={"background-green"}
                 text={"Start a Body Composition"}
-                onClick={startNewBodyComposition} />
+                onClick={() => setModalOpen(true)} />
             <Modal
-                open={open}
-                onClose={handleClose}
-                sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}
-            >
-                <Sheet variant="outlined" className="full-page-modal-container">
-                    <BodyDetails
-                        body={tempBodyComposition}
-                        onUpdate={setTempBodyComposition}
-                        onClose={handleClose}
-                        isOpen={detailsModalOpen} />
-                    <Stack spacing={2} direction="row" className="horizontal-stack" >
-                        <BasicConfirmationModal
-                            buttonStyle={"background-orange"}
-                            openModalButtonLabel={"Cancel"}
-                            modalHeader={"Cancel Body Composition"}
-                            modalBody={"Are you sure you want to cancel your body composition log?"}
-                            modalConfirmationButtonLabel={"Cancel Body Composition"}
-                            actionOnClick={handleClose}
-                        />
-                        <BasicConfirmationModal
-                            buttonStyle={"background-green"}
-                            openModalButtonLabel={"Finish"}
-                            modalHeader={"Finish Body Composition"}
-                            modalBody={"Are you sure you want to finish your body composition log?"}
-                            modalConfirmationButtonLabel={"Finish Body Composition"}
-                            actionOnClick={finishBodyCompositionOnClick}
-                        />
+                open={modalOpen}
+                onClose={handleModalClose}
+                sx={{
+                    width: "100%",
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                }} >
+                <Sheet
+                    variant="outlined"
+                    sx={{ width: "90%", borderRadius: "md", p: 3, boxShadow: "lg" }}>
+                    {/* On invalid empty attempt */}
+                    <ErrorSnackbar
+                        isOpen={snackbarOpen}
+                        snackbarMessage={snackbarMessage}
+                        onClose={handleSnackbarClose} />
+                    <Stack className="w-100" direction="column" alignItems="center">
+                        <Typography
+                            component="h2"
+                            id="modal-title"
+                            level="h4"
+                            className="header-25"
+                            mb={1} >
+                            New Body Composition
+                        </Typography>
+                        <Grid id="modal-desc" className="text-center w-100">
+                            <Stack spacing={2} className="input-container my-2 text-start w-100 mb-4">
+                                <LocalizationProvider dateAdapter={AdapterDateFns}>
+                                    <div>
+                                        <RequiredInputLabel label={"Date"} />
+                                        <DatePicker
+                                            value={tempDetails.date}
+                                            className="time-picker"
+                                            onChange={(newDate) =>
+                                                setTempDetails({ ...tempDetails, date: newDate })
+                                            }
+                                        />
+                                    </div>
+                                </LocalizationProvider>
+                                <TextNumberInputWithLabel
+                                    bindValue={tempDetails.weight}
+                                    label={"Weight (lbs)"}
+                                    placeholder={"Enter value"}
+                                    onInputChange={(e) =>
+                                        setTempDetails({ ...tempDetails, weight: e.target.value })
+                                    }
+                                />
+                                <TextNumberInputWithLabel
+                                    bindValue={tempDetails.waistCircumference}
+                                    label={"Waist Circumference (cm)"}
+                                    placeholder={"Enter value"}
+                                    onInputChange={(e) =>
+                                        setTempDetails({ ...tempDetails, waistCircumference: e.target.value })
+                                    }
+                                />
+                                <TextNumberInputWithLabel
+                                    bindValue={tempDetails.hipCircumference}
+                                    label={"Hip Circumference (cm)"}
+                                    placeholder={"Enter value"}
+                                    onInputChange={(e) =>
+                                        setTempDetails({ ...tempDetails, hipCircumference: e.target.value })
+                                    }
+                                />
+                                <TextNumberInputWithLabel
+                                    bindValue={tempDetails.armCircumference}
+                                    label={"Arm Circumference (cm)"}
+                                    placeholder={"Enter value"}
+                                    onInputChange={(e) =>
+                                        setTempDetails({ ...tempDetails, armCircumference: e.target.value })
+                                    }
+                                />
+                                <TextNumberInputWithLabel
+                                    bindValue={tempDetails.thighCircumference}
+                                    label={"Thigh Circumference (cm)"}
+                                    placeholder={"Enter value"}
+                                    onInputChange={(e) =>
+                                        setTempDetails({ ...tempDetails, thighCircumference: e.target.value })
+                                    }
+                                />
+                            </Stack>
+                        </Grid>
+                        <Stack spacing={20} direction="row" className="horizontal-stack">
+                            <BasicConfirmationModal
+                                buttonStyle={"background-orange"}
+                                openModalButtonLabel={"Cancel"}
+                                modalHeader={"Cancel Body Composition"}
+                                modalBody={"Are you sure you want to cancel tracking a new body composition?"}
+                                modalConfirmationButtonLabel={"Cancel"}
+                                actionOnClick={handleCancel}
+                            />
+                            <BasicConfirmationModal
+                                buttonStyle={"background-green"}
+                                openModalButtonLabel={"Save"}
+                                modalHeader={"Save Body Composition"}
+                                modalBody={"Are you sure you want to save this body composition?"}
+                                modalConfirmationButtonLabel={"Save"}
+                                actionOnClick={handleModalSave}
+                            />
+                        </Stack>
                     </Stack>
                 </Sheet>
             </Modal>
             <Stack spacing={1} className='my-2'>
                 <Typography className='general-label'>Body Composition History</Typography>
-                {userBodyComposition.length > 0 ? (
-                    userBodyComposition.map(composition => (
-                        <BodyCompositionCard key={composition.id} body={composition} />
-                    ))
-                ) : (
-                    <Container style={"background-purple-light p-3 mb-5"} children={
-                        <Typography>No body composition data tracked.</Typography>
-                    } />
-                )}
+                <Container style={"background-purple-light p-3 pb-0 mb-5"} children={
+                    userBodyComposition.length > 0 ? (
+                        userBodyComposition.map(composition => (
+                            <BodyCompositionCard key={composition.id} body={composition} />
+                        ))
+                    ) : (
+                        <Container style={"background-purple-light p-3 mb-5"}>
+                            <Typography>No body composition data tracked.</Typography>
+                        </Container>
+                    )
+                } />
+
             </Stack>
         </>
     );
