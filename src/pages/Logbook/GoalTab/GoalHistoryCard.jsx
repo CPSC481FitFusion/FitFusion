02ec60382bd117d97f02cfeb82e7b4d6da156dbf
racherene <rachelveronikarenegado@gmail.com>
2023-12-05 05:36:00 -0700

    Add background colour changing on checked

--- src/pages/Logbook/GoalTab/GoalHistoryCard.jsx
+++ src/pages/Logbook/GoalTab/GoalHistoryCard.jsx
@@ -1,22 +1,21 @@
 import { Stack, Typography } from "@mui/joy";
 import { Checkbox } from "@mui/material";
 import { green } from "@mui/material/colors";
-import React from "react";
+import React, { useState } from "react";
 import Container from "../../../components/Container";
 import RemoveConfirmationModal from "../../../components/Modals/RemoveConfirmationModal";
 
-export const GoalHistoryCard = (goal) => {
-    const { name, description, deadline } = goal;
-    const [checked, setChecked] = React.useState(false);
-    let backgroundColor = "d-flex align-items-start";
+export const GoalHistoryCard = ({ name, description, deadline }) => {
+    const [checked, setChecked] = useState(false);
+    const [backgroundColor, setBackgroundColor] = useState("d-flex align-items-start");
 
     const handleChange = (event) => {
         setChecked(event.target.checked);
-        console.log('checking now: ' + checked);
-        if (checked === true) {
-            backgroundColor = "d-flex align-items-start background-green-light";
+        if (event.target.checked) {
+            setBackgroundColor("d-flex align-items-start background-green-light");
+        } else {
+            setBackgroundColor("d-flex align-items-start");
         }
-        backgroundColor = "d-flex align-items-start";
     };
 
     return (
@@ -47,18 +46,19 @@ export const GoalHistoryCard = (goal) => {
                             </Typography>
                         </Stack>
                     </Stack>
-                    <RemoveConfirmationModal
-                        modalHeader={"Removing " + name}
-                        modalBody={(
-                            <Typography>
-                                Are you sure you want to remove the goal from your goal history?
-                            </Typography>
-                        )}
-                        onRemoveClick={() => { }}
-                    />
+                    <Stack className={"d-flex align-items-start"}>
+                        <RemoveConfirmationModal
+                            modalHeader={"Removing " + name}
+                            modalBody={(
+                                <Typography>
+                                    Are you sure you want to remove the goal from your goal history?
+                                </Typography>
+                            )}
+                            onRemoveClick={() => { }}
+                        />
+                    </Stack>
                 </Stack>
-
             </>
         } />
     );
-}
+};
